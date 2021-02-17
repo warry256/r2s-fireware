99c04d701e5db73295b2065c732c81f0d391276c
warry256 <54980230+warry256@users.noreply.github.com>
2021-02-18 01:27:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# r2sflash
+git clone https://github.com/songchenwen/nanopi-r2s/tree/master/luci-app-r2sflasher
